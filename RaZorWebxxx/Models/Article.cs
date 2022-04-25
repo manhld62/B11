41df65e3e0c2fe75db070cdc013a612484fae0fb
manhld62 <38492152+manhld62@users.noreply.github.com>
2022-04-25 22:45:06 +0700

    first commit

---
+++ RaZorWebxxx/Models/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RaZorWebxxx.Models
+{
+    public class Article
+    {
+        public int ID { set; get; }
+        public string Title { set; get; }
+        public DateTime Created { set; get; }
+        public string Content { set; get; }
+
+    }
+}
---
+++ RaZorWebxxx/Models/MyBlogContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace RaZorWebxxx.Models
+{
+    public class MyBlogContext : DbContext
+    {
+        public MyBlogContext(DbContextOptions<MyBlogContext> options) : base(options)
+        {
+
+        }
+        protected override void  OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            base.OnConfiguring(builder);
+        }
+        protected override void  OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Article> articles { set; get; }
+    }
+}
